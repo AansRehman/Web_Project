9389c538e102bf7ef60d8c028e4135949266cfd5
AansRehman <106799284+AansRehman@users.noreply.github.com>
2023-06-05 14:41:03 +0500

    frontend done - requires more changes

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocioGram
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
